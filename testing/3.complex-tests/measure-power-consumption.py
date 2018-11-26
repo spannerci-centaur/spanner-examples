66aeed86f9cf805d02e6e7a39283f3fe2ce073b9
spannerci-centaur <45310673+spannerci-centaur@users.noreply.github.com>
2018-11-26 11:01:52 +0200

    Update measure-power-consumption.py

--- testing/3.complex-tests/measure-power-consumption.py
+++ testing/3.complex-tests/measure-power-consumption.py
@@ -25,7 +25,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-testboard = Testboard("testboard_name")
+testboard = Testboard("memas")
 # Our Testboard's D3 Pin is connected to a power switching circuit that controls
 # the power going to the device. When we toggle it HIGH, the device will be
 # powered, when LOW, the device will shut down
@@ -45,13 +45,13 @@ def measure_power_consumption():
     # The device runs some initialization actions in the beginning, which are
     # not indicative of the true power consumption. Therefore we wait for a
     # while for the initial conditions to pass
-    time.sleep(90)
+    time.sleep(3)
 
     # Start measuring power consumption
     testboard.startPowerMeasurement()
 
     # Measure for 5 minutes
-    time.sleep(5*60)
+    time.sleep(30)
 
     # Stop measuring power consumption
     testboard.stopPowerMeasurement()
